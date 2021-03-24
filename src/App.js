4bfffb1c59ee8ca3b7ee451339e2cacc91357060
choijangho <jhchoi1115@gmail.com>
2021-03-25 02:11:04 +0900

    Apply : GlobalStyle , CountryTablePage 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import CountryTablePage from "./components/pages/CountryTablePage";
+import { GlobalStyle } from "./style/GlobalStyle";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <GlobalStyle />
+      <CountryTablePage />
+    </div>
+  );
 };
 
 export default App;
