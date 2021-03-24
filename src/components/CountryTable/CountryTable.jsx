d8115869198f6140e036771eb0969243e9c652bc
choijangho <jhchoi1115@gmail.com>
2021-03-25 02:10:11 +0900

    Feat : table 레이아웃 구성
    - CountryTablePage
    	- 전체 화면을 구성하는 컴포넌트
    
    - CountryTableList
            - table list를 렌더링하는 컴포넌트
    
    - CountryTable
            - table을 구성하는 가장 기초적인 cell 형태의 컴포넌트

---
+++ src/components/CountryTable/CountryTable.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../moduleComponent/Button";
+
+const CountryTable = ({ name, alpha2Code, callingCode, capital, region }) => {
+  return (
+    <CountryTableWrapper>
+      <td>{name}</td>
+      <td>{alpha2Code}</td>
+      <td>{callingCode}</td>
+      <td>{capital}</td>
+      <td>{region}</td>
+
+      <td>
+        <Button width={"100%"} onClick={() => {}}>
+          Delete
+        </Button>
+      </td>
+    </CountryTableWrapper>
+  );
+};
+
+const CountryTableWrapper = styled.tr`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 12px;
+
+  td {
+    display: flex;
+    align-items: flex-start;
+
+    width: 16%;
+    padding: 10px;
+    font-size: 1.4rem;
+  }
+`;
+
+export default CountryTable;
