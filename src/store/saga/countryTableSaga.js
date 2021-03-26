28b8b2fa014beb8df7b22b65cefc4c1a7c05e02a
choijangho <jhchoi1115@gmail.com>
2021-03-26 18:24:14 +0900

    Add : countryTableSaga에서 받아온 데이터에 shorid 적용

--- src/store/saga/countryTableSaga.js
+++ src/store/saga/countryTableSaga.js
@@ -6,12 +6,17 @@ import {
 } from "../module/countryTable";
 
 import { getCountryTable } from "../../api/countryTable";
+import shortid from "shortid";
 
 function* getCountryTableLoad() {
   try {
     const { data } = yield call(getCountryTable);
 
-    yield put(getCountryTableSuccessAction(data));
+    yield put(
+      getCountryTableSuccessAction(
+        data.map((table) => ({ tableId: shortid.generate(), ...table }))
+      )
+    );
   } catch (err) {
     yield put(getCountryTableFailureAction(err));
   }
