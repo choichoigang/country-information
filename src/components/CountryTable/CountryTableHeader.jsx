5bffdd20450ba150045049343230609c94d1d262
choijangho <jhchoi1115@gmail.com>
2021-03-26 18:27:04 +0900

    Rebase : SortButton component moduleComponent 폴더로 이동

--- src/components/CountryTable/CountryTableHeader.jsx
+++ src/components/CountryTable/CountryTableHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import SortButton from "./SortButton";
+import SortButton from "../moduleComponent/SortButton";
 import { reOrderCountryTableAction } from "../../store/module/countryTable";
 
 const CountryTableHeader = () => {
